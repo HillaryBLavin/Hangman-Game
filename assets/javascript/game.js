fb4af1b7bf68ffbbaea194a29390838680cb3bd6
Hillary Lavin <hillarylavin@Hillarys-MacBook-Pro.local>
2018-03-01 13:45:27 -0500

    Pseudocode javascript for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// Pick a random word
+
+
+// Display "blanks" for each letter of the chosen word
+
+
+
+// Display buttons featuring letters of the alphabet 
+
+
+// Display "guesses" counter
+
+
+
+// Register the keys pressed (letters guessed) by the user
+
+
+
+// Show that the guess is either correct or not correct
+
+
+
+// If guess is correct, make that letter appear in the appropriate blank(s) in the chosen word
+
+
+// If guess is incorrect, decrement the "guesses" counter
+
+
+// If "guesses" counter reaches 0, game over
+
+
+// If user guesses word correctly, they win!
